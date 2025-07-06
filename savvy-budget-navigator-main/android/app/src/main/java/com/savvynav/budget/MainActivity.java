4dca99a5a9af2e42e0d5723a9202e76894556051
Keith Ngaira <keithngaira5@gmail.com>
2025-07-06 22:29:28 +0300

    Initial commit

---
+++ savvy-budget-navigator-main/android/app/src/main/java/com/savvynav/budget/MainActivity.java
@@ -0,0 +1,5 @@
+package com.savvynav.budget;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- savvy-budget-navigator-main/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.savvynav.budget',
-  appName: 'Savvy Budget Navigator',
-  webDir: 'dist'
-};
-
-export default config;
